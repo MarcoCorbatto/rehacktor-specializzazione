377ea1f86ad1efef32a9a6cf5e00baea790f2880
Marco Corbatto <marcostudi2323@gmail.com>
2025-05-29 19:48:26 +0200

    rehaktor

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer"
+
+function App() {
+  return (
+  
+    <>
+    
+    <Header />
+    
+    <Footer />
+    
+    </>
+   
+  );
+}
+
+export default App;
