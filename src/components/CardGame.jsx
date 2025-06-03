c64ec8e3c22c83be36729cfa0d8b02c7a87de0a5
Marco Corbatto <marcostudi2323@gmail.com>
2025-06-03 21:51:38 +0200

    cardstile aggiornato

--- src/components/CardGame.jsx
+++ src/components/CardGame.jsx
@@ -1,17 +1,30 @@
 import LazyLoadGameImage from "./LazyLoadGameImage";
 
 export default function CardGame({ game }) {
-  const genres = game.genres.map((genre) => genre.name).join(", ");
-
-  const { background_image: image } = game;
-
   return (
-    <article key={game.id}>
-      <LazyLoadGameImage image={image} />
-      <strong>{game.name}</strong>
-      <small>{genres}</small>
-      <p>{game.released}</p>
-      <button>Visita il gioco</button>
-    </article>
+    <div className="card bg-base-100 w-96 shadow-sm">
+      <figure>
+        <LazyLoadGameImage
+          image={game.background_image}
+          alt={game.name}
+          className="h-48 w-full object-cover"
+        />
+      </figure>
+      <div className="card-body">
+        <h2 className="card-title">
+          {game.name}
+          <div className="badge badge-secondary">NEW</div>
+        </h2>
+        <p>Uscita: {game.released}</p>
+        <div className="card-actions justify-end">
+          {game.genres?.map((genre) => (
+            <div key={genre.id} className="badge badge-outline">
+              {genre.name}
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
   );
 }
+
